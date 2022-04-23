1f23486fe2bd87a279e2ead60d0b715b95c2f61c
Ivy <64839317+ivy0305@users.noreply.github.com>
2022-04-23 15:10:34 +0800

    data

--- generate_coords.py
+++ generate_coords.py
@@ -1,6 +1,6 @@
 import random
-lat = -31.977710
-lng = 115.816484
+lat = -31.97747
+lng = 115.81629
 def generate_coord(coord: float):
 	return '{0:.6f}'.format(coord + random.randint(-99, 99)/10000000)
 for _ in range(150):
