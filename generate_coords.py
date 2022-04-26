a625cceb910f17b8a3bd3c1dcbc52023c242536c
Kaiqi Liang <kaiqi.liang@outlook.com>
2022-04-27 00:03:51 +0800

    Data cleaning and plotting

--- generate_coords.py
+++
@@ -1,7 +0,0 @@
-import random
-lat = -31.97747
-lng = 115.81629
-def generate_coord(coord: float):
-	return '{0:.6f}'.format(coord + random.randint(-99, 99)/10000000)
-for _ in range(150):
-	print(f'{generate_coord(lat)},{generate_coord(lng)}')
